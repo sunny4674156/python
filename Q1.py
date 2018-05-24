9b4a0d24deda33fda167127eac150b027678ced9
sunny4674156 <h4674156@gmail.com>
2018-05-24 16:37:50 +0800

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -2,33 +2,22 @@
 """
 題目：
 
-請使用選擇敘述撰寫一程式，讓使用者輸入兩個整數a,b，然後再輸入
-一算術邏輯子(+,-,*,/,//,%)，輸出這兩個數以及其經過運算後的結果
+請撰寫一程式，輸入一圓半徑，並加以計算此圓之面積和周長
+最後請印出此圓的半徑、面積、周長
+
+*提示：輸出浮點數到小數點第二位
 
 """
 
-A = int(input("請輸入第一個數字："))
-B = int(input("請輸入第二個數字："))
-C = str(input("請輸入算術運算子："))
-print("第一個數字為：", A)
-print("第二個數字為：", B)
-print("算術運算子為：", C)
+import math  
 
-if  C == "+" :
-    Ans= A+B
-elif  C == "-" : 
-    Ans = A-B
-elif  C == "*" : 
-    Ans = A*B
-elif  C == "/" : 
-    ans = A/B
+A = eval(input("請輸入圓半徑："))
+print("圓半徑= %.2f"  %  A )
+B = (2*A)*math.pi
+print("圓周長： %.2f"  %  B)
+C = A*A*math.pi
+print("圓面積： %.2f"  %  C)
 
-elif  C == "//" : 
-    Ans = A//B
-    
-elif  C == "%" : 
-    Ans = A%B 
-print("答案：", Ans)
 
 
 
